b7d39c0992ebca7d6eb7bcc087588bfce8230a07
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-01-14 18:21:44 -0600

    feat: data

---
+++ data/data.ts
@@ -0,0 +1,16 @@
+const data = [
+    {
+        "name": "cerveza1",
+        "price": "11.2"
+    },
+        {
+        "name": "cerveza2",
+        "price": "12.2"
+    },
+        {
+        "name": "cerveza3",
+        "price": "13.3"
+    }
+
+
+]
